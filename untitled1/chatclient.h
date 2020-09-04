4bcbb1e86878eab5c26a86b32d389c739c9c41aa
Ghosttty <67Rus.67Rus@gmail.com>
2020-09-04 16:42:19 +0300

    Create chat

---
+++ untitled1/chatclient.h
@@ -0,0 +1,37 @@
+#ifndef CHATCLIENT_H
+#define CHATCLIENT_H
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+class ChatClient:public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(ChatClient)
+
+public:
+    explicit ChatClient(QObject* parrent =nullptr);
+public slots:
+    void connectToServer(const QHostAddress &adress,quint16 port);
+    void login(const QString &userName);
+    void sendMessage(const QString &text);
+    void disconnectFromHost();
+private slots:
+    void onReadyRead();
+signals:
+    void connected();
+    void loggedIn();
+    void loginError(const QString &reason);
+    void disconnected();
+    void messageReceived(const QString &sender,const QString &text);
+    void error(QAbstractSocket::SocketError socketerror);
+    void userJoined(const QString &userName);
+    void userLeft(const QString &username);
+private:
+    QTcpSocket *m_clientSocket;
+    bool m_loggedIn;
+    void jsonReceived(const QJsonObject &doc);
+
+};
+
+#endif // CHATCLIENT_H
